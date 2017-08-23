0282a79b1bf220999f617eca91a5f3f0b2250dce
YANG WANG <yang.wang.dec@gmail.com>
2017-08-23 10:58:09 -0400

    vertex shader sphere

---
+++ Assets/CreateMesh.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateMesh : MonoBehaviour {
+    public Vector3[] newVertices;
+    public Vector2[] newUV;
+    public int[] newTriangles;
+
+	// Use this for initialization
+	void Start () {
+        //Mesh mesh = new Mesh();
+        //GetComponent<MeshFilter>().mesh = mesh;
+        //mesh.Clear();
+        //mesh.vertices = newVertices;
+        //mesh.uv = newUV;
+        //mesh.triangles = newTriangles;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+        Vector3[] normals = mesh.normals;
+
+        for (int i = 0; i < vertices.Length;i++){
+            vertices[i] += new Vector3(Mathf.PerlinNoise(vertices[i].x, vertices[i].y) - 0.5f, Mathf.PerlinNoise(vertices[i].x, vertices[i].y) - 0.5f, Mathf.PerlinNoise(vertices[i].x, vertices[i].y) - 0.5f);
+        }
+        mesh.vertices = vertices;
+		
+	}
+}
